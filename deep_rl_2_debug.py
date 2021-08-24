0c736b7518d1193b0bd7b041a2aad0d0fbda854e
Aaron C <acrookes330@gmail.com>
2021-08-24 13:44:37 -0400

    working code commit

--- deep_rl_2_debug.py
+++ deep_rl_2_debug.py
@@ -1,7 +1,7 @@
 # Deep Q network using tensorflow2 and keras
 # outputs of DQN correspond directly to the actions you can take (they are the q values)
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.optimizers import Adam
@@ -15,7 +15,7 @@ from tqdm import tqdm
 import os
 import cv2
 
-
+LOAD_MODEL = None
 
 MODEL_NAME = "256x2"
 REPLAY_MEMORY_SIZE = 50_000 # can use _ in place of commas, sweet
@@ -197,93 +197,114 @@ class BlobEnv:
 
 
 
+# Agent class
 class DQNAgent:
     def __init__(self):
 
-        #initially the model will fit to random values, which is basically useless. so we have two models, and re-update the weights of target model
-
-        # main model - this is trained with .fit every step
+        # Main model
         self.model = self.create_model()
 
-        # Target model - this is what we .predict against every step
+        # Target network
         self.target_model = self.create_model()
         self.target_model.set_weights(self.model.get_weights())
 
-        # to make the batch size greater than just one step, we will use a random sample from the replay memory array to increase stability
-        self.replay_memory = deque(maxlen = REPLAY_MEMORY_SIZE)
+        # An array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
 
-        self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{MODEL_NAME}-{int(time.time())}")
+        # Custom tensorboard object
+        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
 
-        self.target_update_counter = 0 # track when we are ready to update the target model
-
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
 
     def create_model(self):
-        #conv net
-        model = Sequential()
 
-        model.add(Conv2D(256, (3,3), input_shape=env.OBSERVATION_SPACE_VALUES))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(2,2))
-        model.add(Dropout(0.2))
+        if LOAD_MODEL is not None:
+            print(f"Loading {LOAD_MODEL}")
+            model = load_model(LOAD_MODEL)
+            print(f"Model {LOAD_MODEL} loaded!")
 
-        model.add(Dense(env.ACTION_SPACE_SIZE, activation="linear"))
-        model.compile(loss="mse", optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+        else:
+            model = Sequential()
+
+            model.add(Conv2D(256, (3, 3), input_shape=env.OBSERVATION_SPACE_VALUES))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Dropout(0.2))
+
+            model.add(Conv2D(256, (3, 3)))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Dropout(0.2))
+
+            model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+            model.add(Dense(64))
+
+            model.add(Dense(env.ACTION_SPACE_SIZE, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
+            model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
         return model
 
+    # Adds step's data to a memory replay array
+    # (observation space, action, reward, new observation space, done)
     def update_replay_memory(self, transition):
         self.replay_memory.append(transition)
-        
-    def get_qs(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
 
+    # Trains main network every step during episode
     def train(self, terminal_state, step):
+
+        # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
             return
 
+        # Get a minibatch of random samples from memory replay table
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
+        # Get current states from minibatch, then query NN model for Q values
         current_states = np.array([transition[0] for transition in minibatch])/255
-        
-
         current_qs_list = self.model.predict(current_states)
-        print("current_states: ", current_states.shape)
-        print(current_qs_list.shape)
 
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
         new_current_states = np.array([transition[3] for transition in minibatch])/255
         future_qs_list = self.target_model.predict(new_current_states)
 
-        X = [] #feature sets
-        y = [] #action we decide to take
+        X = []
+        y = []
 
-        #calculate the learned value to keep track of q's
+        # Now we need to enumerate our batches
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
             if not done:
                 max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT*max_future_q
+                new_q = reward + DISCOUNT * max_future_q
             else:
                 new_q = reward
 
-            #after you update the q value, you need to re-fit the neural net
+            # Update Q value for given state
             current_qs = current_qs_list[index]
-            print("index: ",index)
-            print("current_qs", current_qs)
-            print("action: ", action)
             current_qs[action] = new_q
 
-            X.append(current_state) # append the current state
-            y.append(current_qs) # the current q values
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
 
-        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+        # Fit on all samples as one batch, log only on terminal state
+        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)#, callbacks=[self.tensorboard] if terminal_state else None)
 
-        #updating to determine if we want to update target_model yet
-        if terminal_state: #if we are at the end
-            self.target_update_counter += 1 # update target counter
+        # Update target network counter every episode
+        if terminal_state:
+            self.target_update_counter += 1
 
-
+        # If counter reaches set value, update target network with weights of main network
         if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_weights(self.model.get_weights()) #set the model weights
-            self.target_update_counter = 0 #reset the counter
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
 
+    # Queries main network for Q values given current observation space (environment state)
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
             
 
 # Own Tensorboard class - written by 3rd party - tensorboard normally writes every fit, but we don't want that
